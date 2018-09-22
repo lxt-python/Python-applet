67c0d4d3bf7b2b8f5b74967d55a9334921a11759
sshilang <37956847+sshilang@users.noreply.github.com>
2018-09-22 10:22:21 +0800

    贝壳网武汉各子地区房源链接存入数据库

---
+++ pafangzi/whsubdistrict.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 21:40:54 2018
+
+@author: Administrator
+"""
+
+import pymysql
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from lxml import etree
+import requests
+
+DB = "whbk"
+base_url = "https://wh.ke.com"
+
+def get_sub_districts():
+    result=[]
+    districts = data
+    for item in districts:
+        distr_name = item[0]
+        distr_url = item[1]
+        r = requests.get(distr_url, verify=False)
+        content = r.content.decode("utf-8")
+        root = etree.HTML(content)
+        subdistr_nodes = root.xpath('.//div[@class="m-filter"]//div[@data-role="ershoufang"]/div')[1].xpath('./a')
+        for node in subdistr_nodes:
+            sub_distr_name = node.text
+            sub_distr_url = base_url + node.attrib["href"]
+            result.append([distr_name,sub_distr_name,sub_distr_url])
+    return result
+
+if __name__ == "__main__":
+    db = pymysql.connect(
+            host="***",       # 数据库主机地址
+            user="***",    # 数据库用户名
+            passwd="***",   # 数据库密码
+            database="***"
+            )
+    cur = db.cursor()
+    selectsql = "select * from whdistricts"
+    cur.execute(selectsql.encode('utf-8'))
+    data = cur.fetchall() #所有
+    disc=get_sub_districts()
+    # 使用cursor()方法获取操作游标 
+    cursor = db.cursor()
+     
+    # SQL 有就更新没有就插入
+    for i in range(len(disc)):
+        sql = "REPLACE INTO whsubdistricts (district,sub_district,url) VALUES('%s', '%s', '%s')"%(disc[i][0],disc[i][1],disc[i][2])
+        try:
+           # 执行SQL语句
+           cursor.execute(sql)
+           # 提交修改
+           db.commit()
+        except:
+           # 发生错误时回滚
+           db.rollback()
+    db.close()
