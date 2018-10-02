8010e45c5daa2ee1fa72411789669f567d169dfa
sshilang <37956847+sshilang@users.noreply.github.com>
2018-10-02 15:15:45 +0800

    聚类可视化

--- pafangzi/whfystatistics10.py
+++ pafangzi/whfystatistics10.py
@@ -13,7 +13,7 @@ from random import sample
 from scipy.spatial.distance import cdist
 #PCA主要是用来数据降维，将高维度的特征映射到低维度的特征，加快机器学习的速度。
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
+from sklearn import preprocessing
 from scipy.spatial import cKDTree
 
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
@@ -65,17 +65,23 @@ def k_means_cluster(points,k,first_centroids=None, predict_method=None):
 def show():
     d = pd.read_excel("./one_hot.xlsx", sheetname='123')
     d = np.array(d)
-    iters, centers, assignments = k_means_cluster(d, 4)
-    
+    scaler = preprocessing.StandardScaler().fit(d)
+    d = scaler.transform(d)
     #以下是将聚类结果可视化出来
     #PCA(n_components=2)表示将4个特征的向量降维到二维，即可以画在平面
     pca_model = PCA(n_components=2)
     #将iris.data转换成标准形式，然后存入reduced_data中
     reduced_data = pca_model.fit_transform(d)
-    #将前面的几何中心点centers也转换成标准形式，然后存入reduced_centers中
-    reduced_centers = pca_model.transform(centers)
+    iters, centers, assignments = k_means_cluster(reduced_data, 8)
+    print(centers, assignments)
+    
+    
+    assignments1 = pd.DataFrame(assignments)
+    print(assignments1)
+    assignments1.to_excel("./one_hot1.xlsx",sheet_name="234",index=False,header=True)
+    
     #h表示间距
-    h = 50
+    h = .2
     #下面求x_min, x_max和y_min, y_max，主要是为了确定坐标轴
     x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
     y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
@@ -84,27 +90,35 @@ def show():
     xx_pt = list(xx.ravel())
     yy_pt = list(yy.ravel())
     xy_pts = np.array([[x,y] for x,y in zip(xx_pt, yy_pt)])
-    mytree = cKDTree(reduced_centers)
+    mytree = cKDTree(centers)
     dist, indexes = mytree.query(xy_pts)
     indexes =indexes.reshape(xx.shape)
     
     #下面使用matplotlib将图给画出来
     plt.clf()
     plt.imshow(indexes, interpolation='nearest', extent=(xx.min(), xx.max(), yy.min(), yy.max()), cmap=plt.cm.Paired, aspect='auto', origin='lower')
-    symbols = ['o', '^', 'D', 's']
-    for i in range(4):
-        temp_group = reduced_data[(i*50) : (50)*(i+1)]
-        plt.plot(temp_group[:, 0], temp_group[:, 1], symbols[i], markersize=10)
-    plt.scatter(reduced_centers[:, 0], reduced_centers[:, 1], marker='x', s=169, linewidths=3, zorder=10)
+    symbols = ['o', '^', 'D', 's', '.', ',', '<',  '*']
+    #sym=[sysmbols[i] for i in assignments]
+    for i in range(8):
+        x=[]
+        y=[]
+        for j in range(assignments.shape[0]):
+            if assignments[j]==i:
+                x.append(reduced_data[j][0])
+                y.append(reduced_data[i][1])
+        plt.plot(x, y, symbols[i], markersize=10)
+        
+    """
+    temp_group = reduced_data[(i*50) : (50)*(i+1)]
+    plt.plot(temp_group[:, 0], temp_group[:, 1], symbols[i], markersize=10)
+    """
+
+    plt.scatter(centers[:, 0], centers[:, 1], marker='x',color='black', s=169, linewidths=3, zorder=10)
     plt.title('K-means clustering')
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
-    plt.legend(loc='lower right')
+    plt.savefig('./whfypca.png')
     plt.show()
-
-
-
-    
 
     
 if __name__ == "__main__":
